889c572ebd25495cca3fc0763442249733b86d8d
aminuubaa <aminuubaa@gmail.com>
2024-06-23 01:03:38 +0200

    adjust the margin of race bar chart

--- dashboard.js
+++ dashboard.js
@@ -331,7 +331,7 @@ function createChart2(parseData) {
 
 
 function createChart3(parseData) {
-    const margin = { top: 50, right: 150, bottom: 100, left: 100 };
+    const margin = { top: 50, right: 150, bottom: 50, left: 50 };
     const width = 700 - margin.left - margin.right;
     const height = 500 - margin.top - margin.bottom;
 
